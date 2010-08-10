9d7389b289e847f511b23453e2e5d61970957064
Lucas Pirola <lucaspirola@gmail.com>
2010-08-10 06:26:40 -0300

    lib help

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'tira-teima2010');
+
+/*
+ * This is the "official" OO way to do it,
+ * BUT $connect_error was broken until PHP 5.2.9 and 5.3.0.
+ */
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+?>
